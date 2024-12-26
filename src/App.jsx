68f41a9394f411f0900631742dd0f2756f714087
AbylayNurtayev <krutyev6@gmail.com>
2024-12-26 11:45:10 +0500

    dsadas

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,22 @@ import { Routes, Route } from 'react-router-dom';
 import { FiltersProvider } from './context/FiltersContext';
 import LogoPage from './pages/LogoPage'
 import CalculatePage from './pages/CalculatePage';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const tg = window.Telegram.WebApp;
+
+    // Расширение до полного экрана
+    tg.expand();
+
+    // Установка события завершения инициализации
+    tg.ready();
+
+    return () => {
+      tg.close(); // Закрытие веб-приложения (при необходимости)
+    };
+  }, []);
   return (
       <FiltersProvider>
         <div className="App flex justify-center items-center">
