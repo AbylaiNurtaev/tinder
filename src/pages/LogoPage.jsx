c63091b7e8aca02c640764b823267a502a60ad0e
AbylayNurtayev <krutyev6@gmail.com>
2024-12-30 16:33:33 +0500

    das

--- src/pages/LogoPage.jsx
+++ src/pages/LogoPage.jsx
@@ -1,28 +1,33 @@
-import React from 'react';
-import Button from '../components/Button';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import Button from '../components/Button'
+import { useNavigate } from 'react-router-dom'
+import axios from '../axios'
 
 function LogoPage() {
+  const [userId, setUserId] = useState()
+  useEffect(() => {
     const initData = window.Telegram.WebApp.initData;
-    const navigate = useNavigate();
-
-    console.log("initData", initData);
-
-    return (
-        <div className='flex flex-col justify-start items-center'>
-            {initData ? (
-                <>
-                    <p>ID пользователя: {initData.id}</p>
-                    {/* Вы можете использовать другие данные, например, имя пользователя */}
-                    <p>Имя пользователя: {initData.first_name}</p>
-                    <img className='mt-[250px]' src="/images/ui/logo.svg" alt="" />
-                </>
-            ) : (
-                "dsa"
-            )}
-            <Button className="mt-[120px] h-[64px] w-[250px]" onClick={() => navigate('/calculate')}>Найти вторую половинку</Button>
-        </div>
-    );
+    axios.post('/api/validate-init-data', {
+      initData
+    })
+    .then(res => res.data)
+    .then(data => {
+      if(data){
+        setUserId(data?.userId)
+      }
+    })
+  }, [])
+    
+    
+    const navigate = useNavigate()
+  return (
+    <div className='flex flex-col justify-start items-center'>
+      {userId ? userId : "userId"}
+        <img className='mt-[250px]' src="/images/ui/logo.svg" alt="" />
+        
+        <Button className="mt-[120px] h-[64px] w-[250px]" onClick={() => navigate('/calculate')}>Найти вторую половинку</Button>
+    </div>
+  )
 }
 
-export default LogoPage;
+export default LogoPage
