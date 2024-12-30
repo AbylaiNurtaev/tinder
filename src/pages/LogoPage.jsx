c953541e9a197593c2ae4b878b9e255b08438812
AbylayNurtayev <krutyev6@gmail.com>
2024-12-30 16:04:15 +0500

    dsaa

--- src/pages/LogoPage.jsx
+++ src/pages/LogoPage.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import Button from '../components/Button'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import Button from '../components/Button';
+import { useNavigate } from 'react-router-dom';
 
 function LogoPage() {
     const initData = window.Telegram.WebApp.initData;
+    const navigate = useNavigate();
+
     console.log("initData", initData);
-    const navigate = useNavigate()
-  return (
-    <div className='flex flex-col justify-start items-center'>
-      {initData ? initData : "dsa"}
-        <img className='mt-[250px]' src="/images/ui/logo.svg" alt="" />
-        
-        <Button className="mt-[120px] h-[64px] w-[250px]" onClick={() => navigate('/calculate')}>Найти вторую половинку</Button>
-    </div>
-  )
+
+    return (
+        <div className='flex flex-col justify-start items-center'>
+            {initData ? (
+                <>
+                    <p>ID пользователя: {initData.id}</p>
+                    {/* Вы можете использовать другие данные, например, имя пользователя */}
+                    <p>Имя пользователя: {initData.first_name}</p>
+                    <img className='mt-[250px]' src="/images/ui/logo.svg" alt="" />
+                </>
+            ) : (
+                "dsa"
+            )}
+            <Button className="mt-[120px] h-[64px] w-[250px]" onClick={() => navigate('/calculate')}>Найти вторую половинку</Button>
+        </div>
+    );
 }
 
-export default LogoPage
+export default LogoPage;
